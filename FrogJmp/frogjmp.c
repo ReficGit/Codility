05296194120111b82e088674ddae1682a4355b67
Maksim Sisov <maksim.sisov@hotmail.com>
2015-11-25 15:41:28 +0200

    Solution to frogjmp task

--- FrogJmp/frogjmp.c
+++ FrogJmp/frogjmp.c
@@ -1,1 +1,12 @@
-#include <stdio.h>
+int solution(int X, int Y, int D) {
+    long dist = Y - X;
+    long jumps = dist/D;    
+    
+    if(X==Y)
+        return 0;
+    
+    if(dist%D == 0)
+        return (jumps);
+    else
+        return (jumps+1);
+}
